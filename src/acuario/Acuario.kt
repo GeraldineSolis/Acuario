d02af3d023ef47e526d458642b27570ff2ecff02
GeraldineSolis <solis.geraldine@tecsup.edu.pe>
2025-08-31 18:44:20 -0500

    Acuario: Herencia Terminado

---
+++ src/acuario/Acuario.kt
@@ -0,0 +1,33 @@
+package acuario
+
+
+open class Acuario (open var largo: Int = 100, open var ancho: Int = 20, open var alto: Int = 40) {
+    open var volumen: Int
+        get() = ancho * alto * largo / 1000
+        set(valor) {
+            alto = (valor * 1000) / (ancho * largo)
+        }
+
+    open val forma = "rectángulo"
+
+    open var agua: Double = 0.0
+        get() = volumen * 0.9
+
+    fun imprimirTamano() {
+        println(forma)
+        println("Ancho: $ancho cm " +
+                "Largo: $largo cm " +
+                "Alto: $alto cm ")
+        println("Volumen: $volumen l Agua: $agua l (${agua/volumen*100.0}% lleno)")
+    }
+
+    constructor(numeroDePeces: Int) : this() {
+        // 2,000 cm^3 por pez + espacio extra para que no se derrame
+        val tanque = numeroDePeces * 2000 * 1.1
+        alto = (tanque / (largo * ancho)).toInt()
+    }
+
+    init {
+        println("inicializando acuario")
+    }
+}
